f241151c4cc2f252f4b3ad5021666d64fdf584e6
Eyerusalem Sahle <58132233+jerrysahle27@users.noreply.github.com>
2024-02-04 14:46:20 +0300

    Time: 117 ms (32.94%), Space: 46.6 MB (6.61%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cs
@@ -0,0 +1,7 @@
+public class Solution {
+    public void Merge(int[] nums1, int m, int[] nums2, int n) {
+        Array.Copy(nums2, 0, nums1, m, n);
+        Array.Sort(nums1);
+        Console.WriteLine(string.Join(", ", nums1));
+    }
+}
